a9ee8480062e9fa9717ec2b4d8aca5f02f7f691a
JunlongLv <962876778@qq.com>
2020-08-31 15:45:10 +0800

    项目初始结构创建

---
+++ gulimall-member/src/test/java/com/shop/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
